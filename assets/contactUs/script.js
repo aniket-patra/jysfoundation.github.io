8ffbbc908983024e5d1bf07910922dd09f35b240
aniket <aniket.howrah@gmail.com>
2021-04-11 11:47:55 +0530

    Update script.js

--- assets/contactUs/script.js
+++ assets/contactUs/script.js
@@ -2,6 +2,6 @@ function nameCopy()
 {
     var name=document.getElementById("form_name").value;
     document.getElementById("nameFiller").value=name;
-    document.getElementById("subjectSetter").value="Message from Github profile.";
+    document.getElementById("subjectSetter").value="Message from J.Y.S. Foundation website";
     localStorage.setItem('userName',name);    
 }
