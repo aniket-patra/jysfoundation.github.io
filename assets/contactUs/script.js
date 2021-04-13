d007df0502fba494264d7976372933178017ade9
aniket <aniket.howrah@gmail.com>
2021-04-13 08:55:13 +0530

    Updates to Contact
    added spinner for submit and minor other changes

--- assets/contactUs/script.js
+++ assets/contactUs/script.js
@@ -1,8 +1,21 @@
-function nameCopy()
-{
+const form = document.getElementById('form');
+
+form.onsubmit = submit;
+
+function submit(event) {
+    postSubmit();
+  }
+
+  function nameCopy()
+{  
     var name=document.getElementById("form_name").value;
     document.getElementById("nameFiller").value=name;
     document.getElementById("subjectSetter").value="Message from J.Y.S. Foundation website";
     localStorage.setItem('userName',name);    
 }
 
+function postSubmit()
+{
+    document.getElementById("submit").disabled=true
+    document.getElementById('spinnerSubmit').style.visibility="visible";
+}
